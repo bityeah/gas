b9cf462507ad1afe3b501d42dfcea3d9d0cc6a26
zhaohg <zhaohg@danqoo.com>
2017-07-28 17:02:16 +0800

    test

--- gas-pay/src/main/java/cn/demo/gas/pay/StartUp.java
+++ gas-pay/src/main/java/cn/demo/gas/pay/StartUp.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by huangjp on 2017/7/18.
  */
 @SpringBootApplication
-@MapperScan("cn.demo.gas.pay.dao.mapper")
+@MapperScan(value = "cn.demo.gas.pay.dao.mapper")
 public class StartUp {
 
     public static void main(String[] args) {
--- gas-pay/src/main/java/cn/demo/gas/pay/controller/UserController.java
+++ gas-pay/src/main/java/cn/demo/gas/pay/controller/UserController.java
@@ -1,14 +1,11 @@
 package cn.demo.gas.pay.controller;
 
 import cn.demo.gas.pay.from.UserSubmit;
-import cn.demo.gas.pay.model.User;
 import cn.demo.gas.pay.service.UserService;
 import cn.demo.gas.pay.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by huangjp on 2017/7/18.
@@ -25,7 +22,7 @@ public class UserController {
     }
 
 
-    @RequestMapping("/user/search")
+    @RequestMapping("/user/list")
     public Result getUserList(UserSubmit submit) {
         return userService.getUserList(submit);
     }
