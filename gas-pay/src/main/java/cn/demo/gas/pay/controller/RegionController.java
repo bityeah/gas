982dc6102b95749d78baa29809f9b133cbfd3345
zhaohg <zhaohg@danqoo.com>
2017-08-02 13:08:31 +0800

    地区

--- gas-pay/src/main/java/cn/demo/gas/pay/controller/RegionController.java
+++ gas-pay/src/main/java/cn/demo/gas/pay/controller/RegionController.java
@@ -1,19 +1,15 @@
 package cn.demo.gas.pay.controller;
 
-import cn.demo.gas.pay.model.Region;
 import cn.demo.gas.pay.service.RegionService;
+import cn.demo.gas.pay.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 省市区
  * Created by zhaohg on 2017/7/26.
  */
-@RequestMapping("/region")
 @RestController
 public class RegionController {
 
@@ -21,8 +17,8 @@ public class RegionController {
     private RegionService regionService;
 
 
-    @RequestMapping("/list")
-    public List<Region> getReginList(@PathVariable int parentId) {
+    @RequestMapping("/region/list")
+    public Result getReginList(int parentId) {
         return regionService.getRegionByParentId(parentId);
     }
 
